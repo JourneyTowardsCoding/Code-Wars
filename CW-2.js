6e5c89c759f5a143ee3103f2795dfb9adb36ce0a
Muhammad Tauhid <journey.towards.coding@gmail.com>
2024-01-20 15:59:46 +0600

    code war 2

---
+++ CW-2.js
@@ -0,0 +1,20 @@
+QUESTION:
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+ANS:
+function repeatStr (n, s) {
+  var str="";
+  for(var i=0; i < n; i++)
+    str+=s;
+    return str;
+  }
+
+
+ANS:
+function repeatStr(n, s){
+  return s.repeat(n);
+}  
